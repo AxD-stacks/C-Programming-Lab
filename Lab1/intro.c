1f2c0a9c7021aa64da93367541146a779c171696
Ayush Dey <ayushdey2908@gmail.com>
2025-10-22 19:04:11 +0530

    Added intro.c
    this is the second program in Lab 1

---
+++ Lab1/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is Ayush Dey\n");
+    printf("My branch iss Mechanical Engineering\n");
+    printf("My roll no is MEB25073\n ");
+    return 0;
+}
