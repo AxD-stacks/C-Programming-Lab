b04276ae786389a73edaf148417722b15ecf2fbb
Ayush Dey <ayushdey2908@gmail.com>
2025-10-24 14:38:01 +0530

    updated code

--- Assignment1/number_summation.c
+++ Assignment1/number_summation.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int num, temp, sum;
+    int num, sum;
     printf("Enter the number:\n");
     scanf("%d", &num);
     temp=num*(num+1);
     sum=temp/2;
     printf("The summation of first %d natural numbers is: %d\n", num, sum);
     return 0;
+
 }
