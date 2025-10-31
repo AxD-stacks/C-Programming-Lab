c8e3a3308fa64fc725d1fe3519d3f45e0a727550
Ayush Dey <ayushdey2908@gmail.com>
2025-10-31 16:46:24 +0530

    Fixed leap year logic to include century years

--- Assignment1/leap_year_checker.c
+++ Assignment1/leap_year_checker.c
@@ -5,10 +5,11 @@ int main()
     printf("Enter year:\n");
     scanf("%d", &year);
     temp=year;
-    if(temp%4==0 && temp%100!=0)
+    if(temp%4==0 && temp%100!=0) || (temp%400==0)
         printf("%d is a leap year.\n", year);
     else
         printf("%d is not a leap year.\n", year);
     return 0;
 
 }
+
